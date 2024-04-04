6aee864b49200a1d240faf750a5dfab3dfd4bcc6
Chaffexd <shanechaffe@gmail.com>
2024-04-04 19:03:45 +0100

    fixed mobile styles

--- app/components/Blog/Post.tsx
+++ app/components/Blog/Post.tsx
@@ -47,7 +47,7 @@ const Post = ({ post }: PostProps) => {
 
   return (
     <article className="relative flex flex-col items-start hover:bg-zinc-50 rounded-lg p-8 dark:hover:bg-zinc-700">
-      <time className="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-white pl-3.5">
+      <time className="relative z-10 order-first mb-3 flex flex-col md:flex-row md:items-center text-sm text-zinc-400 dark:text-white pl-3.5">
         <span className="absolute inset-y-0 left-0 flex items-center">
           <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
         </span>
