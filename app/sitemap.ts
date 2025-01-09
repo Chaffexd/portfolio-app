7453bd70e404a84151ee063f17c61a4273ac37b5
Chaffexd <shanechaffe@gmail.com>
2025-01-09 00:08:01 +0000

    adjust sitemap rescanning

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,32 +7,32 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts: MetadataRoute.Sitemap = blogPosts.items.map((post) => ({
     url: `https://www.chaffe.dev/blog/${post.fields.slug}`,
     lastModified: post.sys.updatedAt,
-    changeFrequency: "weekly",
+    changeFrequency: "hourly",
     priority: 1,
   }));
 
   return [
     {
       url: "https://www.chaffe.dev",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "weekly",
       priority: 1,
     },
     {
       url: "https://www.chaffe.dev/about",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://www.chaffe.dev/blog",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "weekly",
       priority: 0.9,
     },
     {
       url: "https://www.chaffe.dev/projects",
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: "weekly",
       priority: 0.7,
     },
